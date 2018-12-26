cc5ae6baa17708e301f2ead001e010093f0cc8ee
holubv <holubvojtech@gmail.com>
2018-12-26 22:42:05 +0100

    Remove unnecessary closure

--- src/FSEdit/DatabaseMiddleware.php
+++ src/FSEdit/DatabaseMiddleware.php
@@ -10,9 +10,7 @@ class DatabaseMiddleware
     public function __construct(App $app)
     {
         $container = $app->getContainer();
-        $container['database'] = function () use ($container) {
-            return new Medoo($container->get('config')->database);
-        };
+        $container['database'] = new Medoo($container->get('config')->database);
     }
 
     public function __invoke($request, $response, $next)
